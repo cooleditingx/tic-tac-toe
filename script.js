60e1e2f0e5866df99f5c4d1c8cbb0b56c9c1116d
cooleditingx <cooleditingx@yahoo.com>
2025-11-13 03:30:40 +0400

    game logic in console complete

--- script.js
+++ script.js
@@ -3,15 +3,12 @@
     let column;
     function GameBoard(){
         let gameBoard = []
-        //let x = 1
         for (let i = 0; i < 3; i++) {
             gameBoard[i] = [];
             for (let j = 0; j < 3; j++) {
                 gameBoard[i][j]= ""
-                //x= x+1
             }
         }
-        gameBoard[2][2] = "O"
         return gameBoard
     }
     function Players(name,token,score){
@@ -21,16 +18,28 @@
     }
     let player1 = new Players("player1","O",0)
     let player2 = new Players("player2","X",0)
-    function playerTurn(){
-        let activePlayer;
-        if (player1.token == "X"){
-            activePlayer = player1.name
-        } else if (player2.token == "X"){
-            activePlayer = player2.name
+    function changeTurn(){
+        if (activePlayer == player2){
+            activePlayer = player1
+        } else {
+            activePlayer = player2
         }
         return activePlayer
     }
-    function validateInput(){
+    function activePlayer(){
+        for (let x = 0; x < 9; x++) {
+            if (x == 0 ){
+                activePlayer = player2
+            } else if (x > 0){
+                activePlayer = changeTurn()
+            }
+        }
+        return activePlayer
+    }
+    //let currentPlayer = activePlayer()
+    let gameBoard = []
+    gameBoard = GameBoard()
+    function validateInput(gameBoard,currentPlayer){
         row = prompt("Enter row number");
         if (row<0 || row>2) {
             console.log("Please enter a valid row and column number (from 0,2,1")
@@ -41,29 +50,65 @@
             console.log("Please enter a valid row and column number (from 0,2,1")
             column = (prompt("Enter column number"));
         }
-        let gameBoard = []
-        gameBoard = GameBoard()
         if (gameBoard[row][column] == ""){
-            gameBoard[row][column] = "X";
+            gameBoard[row][column] = currentPlayer.token;
             return gameBoard
         } else {
             console.log("This cell is taken please enter a new cell")
-            return validateInput()
+            return validateInput(gameBoard)
         }
     }
-    function updateDisplay(){
-        let gameBoard = GameBoard()
-        let board = validateInput()
-        gameBoard.forEach(row => {
-            row.forEach((element,index) => {
-                if(gameBoard[index] = ""){
-                    gameBoard[index] = board[index]
-                }
-            })
-        })
-        return gameBoard
+    function checkWinner(x){
+        if ((x[0][0] == player2.token && x[0][1] == player2.token && x[0][2] == player2.token) || (x[1][0] == player2.token && x[1][1] == player2.token && x[1][2] == player2.token) || (x[2][0] == player2.token && x[2][1] == player2.token && x[2][2] == player2.token) || (x[0][0] == player2.token && x[1][0] == player2.token && x[2][0] == player2.token) || (x[0][1] == player2.token && x[1][1] == player2.token && x[2][1] == player2.token) || (x[0][2] == player2.token && x[1][2] == player2.token && x[2][2] == player2.token) || (x[0][0] == player2.token && x[1][1] == player2.token && x[2][2] == player2.token) || (x[2][0] == player2.token && x[1][1] == player2.token && x[0][2] == player2.token)){
+            return true
+        } else if ((x[0][0] == player1.token && x[0][1] == player1.token && x[0][2] == player1.token) || (x[1][0] == player1.token && x[1][1] == player1.token && x[1][2] == player1.token) || (x[2][0] == player1.token && x[2][1] == player1.token && x[2][2] == player1.token) || (x[0][0] == player1.token && x[1][0] == player1.token && x[2][0] == player1.token) || (x[0][1] == player1.token && x[1][1] == player1.token && x[2][1] == player1.token) || (x[0][2] == player1.token && x[1][2] == player1.token && x[2][2] == player1.token) || (x[0][0] == player1.token && x[1][1] == player1.token && x[2][2] == player1.token) || (x[2][0] == player1.token && x[1][1] == player1.token && x[0][2] == player1.token)){
+            return true
+        } else{
+            return false
+        }
     }
-    console.log(updateDisplay())
-    console.log(updateDisplay())
-    console.log(GameBoard())
+    function checkDraw(moves){
+        if (checkWinner(gameBoard) == false && moves == 8){
+            return true
+        }
+    }
+    function playGame(){
+        let gameBoard = GameBoard()//initializing gameboard
+        console.log("1 | 2 | 3")//print board
+        console.log("4 | 5 | 6")// print board
+        console.log("7 | 8 | 9")// print board
+        let i = 0; 
+        while (checkWinner(gameBoard) == false && i<=8){
+            if (i == 0 ){
+                activePlayer = player2
+                console.log("This is player",player2.token, "turn")
+                validateInput(gameBoard,activePlayer)
+                console.log(`
+                    ${gameBoard[0][0]} | ${gameBoard[0][1]} | ${gameBoard[0][2]}
+                    ---------
+                    ${gameBoard[1][0]} | ${gameBoard[1][1]} | ${gameBoard[1][2]}
+                    ---------
+                    ${gameBoard[2][0]} | ${gameBoard[2][1]} | ${gameBoard[2][2]}
+                `)
+            } else if (i > 0){
+                activePlayer = changeTurn()
+                console.log("This is player",activePlayer.token, "turn")
+                validateInput(gameBoard,activePlayer)
+                console.log(`
+                    ${gameBoard[0][0]} | ${gameBoard[0][1]} | ${gameBoard[0][2]}
+                    ---------
+                    ${gameBoard[1][0]} | ${gameBoard[1][1]} | ${gameBoard[1][2]}
+                    ---------
+                    ${gameBoard[2][0]} | ${gameBoard[2][1]} | ${gameBoard[2][2]}
+                `)
+                if (checkWinner(gameBoard) == true){
+                    console.log(`The winner is player ${activePlayer.token}`)
+                } else if (checkDraw(i) == true){
+                    console.log("It's a tie")
+                }
+            }
+            i = i +1
+        }
+    }
+    playGame()
 })()
